36d7e1d93e59b0ae1e935ad7e2d41236c2b0f135
MarkDestroyer <markkir123@gmail.com>
2021-08-17 23:33:55 +0300

    swift
    make homework for lesson-2(partly).

--- lesson 1/lesson-1/Cells/NewsTableViewCell.swift
+++ lesson 1/lesson-1/Cells/NewsTableViewCell.swift
@@ -1,7 +1,7 @@
 import UIKit
 import Kingfisher
 import ImageViewer_swift
-import Alamofire
+import SDWebImage
 
 class NewsTableViewCell: UITableViewCell {
 
@@ -20,73 +20,73 @@ class NewsTableViewCell: UITableViewCell {
     @IBAction func setMainLike(_ sender: Any) {
         (sender as! LikeButton).like()
     }
-        override func prepareForReuse() {
-            super.prepareForReuse()
-            mainAuthorImage.image = UIImage(named: "user_default")
-            mainAuthorName.text = nil
-            publicationDate.text = nil
-            self.accessoryType = .none
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        mainAuthorImage.image = UIImage(named: "user_default")
+        mainAuthorName.text = nil
+        publicationDate.text = nil
+        self.accessoryType = .none
+    }
+    
+    private var photos = [URL]()
+    
+    func renderCell(model: NewsCellModel) {
+        
+        if let url = URL(string: model.mainAuthorImage) {
+            mainAuthorImage.kf.setImage(with: url)
         }
+        mainAuthorName.text = model.mainAuthorName
+        publicationDate.text = model.publicationDate
+        publicationText.text = model.publicationText
+        publicationLikeButton.likeCount = model.publicationLikeButtonCount
+        publicationLikeButton.liked = model.publicationLikeButtonStatus
+        publicationCommentButton.setTitle(model.publicationCommentButton, for: .normal)
+        publicationCommentButton.tintColor = .darkGray
+        publicationForwardButton.setTitle(model.publicationForwardButton, for: .normal)
+        publicationForwardButton.tintColor = .darkGray
+        publicationNumberOfViews.setTitle(model.publicationNumberOfViews, for: .normal)
+        publicationNumberOfViews.tintColor = .darkGray
+        newsCollectionView.isHidden = model.newsCollectionViewIsEmpty
         
-        private var photos = [URL]()
-        
-        func renderCell(model: NewsCellModel) {
-            
-            if let url = URL(string: model.mainAuthorImage) {
-                mainAuthorImage.kf.setImage(with: url)
-            }
-            mainAuthorName.text = model.mainAuthorName
-            publicationDate.text = model.publicationDate
-            publicationText.text = model.publicationText
-            publicationLikeButton.likeCount = model.publicationLikeButtonCount
-            publicationLikeButton.liked = model.publicationLikeButtonStatus
-            publicationCommentButton.setTitle(model.publicationCommentButton, for: .normal)
-            publicationCommentButton.tintColor = .darkGray
-            publicationForwardButton.setTitle(model.publicationForwardButton, for: .normal)
-            publicationForwardButton.tintColor = .darkGray
-            publicationNumberOfViews.setTitle(model.publicationNumberOfViews, for: .normal)
-            publicationNumberOfViews.tintColor = .darkGray
-            newsCollectionView.isHidden = model.newsCollectionViewIsEmpty
-            
-            if newsCollectionView.isHidden == false {
-                self.photos = model.photoCollection
-                newsCollectionView.register(UINib(nibName:"NewsCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "NewsCollectionViewCell")
-                newsCollectionView.reloadData()
-                newsCollectionView.delegate = self
-                newsCollectionView.dataSource = self
-            }
+        if newsCollectionView.isHidden == false {
+            self.photos = model.photoCollection
+            newsCollectionView.register(UINib(nibName:"NewsCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "NewsCollectionViewCell")
+            newsCollectionView.reloadData()
+            newsCollectionView.delegate = self
+            newsCollectionView.dataSource = self
         }
     }
+}
 
-    extension NewsTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
-        
-        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-            return photos.count
-        }
-        
-        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-            
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "NewsCollectionViewCell", for: indexPath) as? NewsCollectionViewCell else { return UICollectionViewCell() }
-            
-            cell.backgroundColor = .clear
-            cell.collectionImage.kf.setImage(with: photos[indexPath.row])
-            
-            // Setup Image Viewer with [URL]
-            let config = UIImage.SymbolConfiguration(pointSize: UIFont.systemFontSize, weight: .bold, scale: .large)
-            if let image = UIImage(systemName: "chevron.left", withConfiguration: config) {
-                let newImage = image.withTintColor(.darkGray, renderingMode: .alwaysOriginal)
-                let options: [ImageViewerOption] = [.closeIcon(newImage)]
-         //       cell.collectionImage.setupImageViewer(urls: photos, initialIndex: indexPath.row, options: options)
-            } else {
-       //         cell.collectionImage.setupImageViewer(urls: photos, initialIndex: indexPath.row)
-            }
-            
-            cell.collectionImage.contentMode = .scaleAspectFill
-            cell.collectionImage.layer.borderWidth = 1
-            cell.collectionImage.layer.borderColor = UIColor.darkGray.cgColor
-            cell.collectionImage.layer.cornerRadius = 10
-            
-            return cell
-        }
+extension NewsTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
     }
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "NewsCollectionViewCell", for: indexPath) as? NewsCollectionViewCell else { return UICollectionViewCell() }
+        
+        cell.backgroundColor = .clear
+        cell.collectionImage.kf.setImage(with: photos[indexPath.row])
+        
+        // Setup Image Viewer with [URL]
+        let config = UIImage.SymbolConfiguration(pointSize: UIFont.systemFontSize, weight: .bold, scale: .large)
+        if let image = UIImage(systemName: "chevron.left", withConfiguration: config) {
+            let newImage = image.withTintColor(.darkGray, renderingMode: .alwaysOriginal)
+            let options: [ImageViewerOption] = [.closeIcon(newImage)]
+            cell.collectionImage.setupImageViewer()
+        } else {
+            cell.collectionImage.setupImageViewer(urls: photos, initialIndex: indexPath.row)
+        }
+        
+        cell.collectionImage.contentMode = .scaleAspectFill
+        cell.collectionImage.layer.borderWidth = 1
+        cell.collectionImage.layer.borderColor = UIColor.darkGray.cgColor
+        cell.collectionImage.layer.cornerRadius = 10
+        
+        return cell
+    }
+}
